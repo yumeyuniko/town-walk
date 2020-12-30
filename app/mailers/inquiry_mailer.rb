b35d69369fc38be538c34891a90615b7ce1debc6
ogatasaori <daigocafe@msn.com>
2020-12-30 18:58:00 +0900

    お問い合わせフォーム

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,2 +1,8 @@
 class InquiryMailer < ApplicationMailer
+  default from: "daigocafe22@gmail.com"   # 送信元アドレス
+
+  def received_email(inquiry)
+    @inquiry = inquiry
+    mail(:to => inquiry.email, :subject => 'お問い合わせを承りました')
+  end
 end
